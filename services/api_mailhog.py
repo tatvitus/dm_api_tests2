59b68fe611bfa9721ea69de51751505fe7dd7a6f
ustinov-gv <ustinov@exportcenter.ru>
2025-08-14 20:07:44 +0300

    добавлен фасад

---
+++ services/api_mailhog.py
@@ -0,0 +1,12 @@
+from restclient.configuration import Configuration
+from api_mailhog.apis.mailhog_api import MailhogApi
+
+
+class MailHogApi:
+    def __init__(
+            self,
+            configuration: Configuration
+            ):
+        self.configuration = configuration
+        self.mailhog_api = MailhogApi(configuration=self.configuration)
+
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,10 +1,10 @@
 from json import loads
 
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
+
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
+from services.dm_api_account import DMApiAccount
+from services.api_mailhog import MailHogApi
 
 import structlog
 
@@ -22,10 +22,11 @@ def test_post_v1_account():
     # Регистарция пользователя
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-    account_api = AccountApi(configuration=dm_api_configuration)
-    login_api = LoginApi(configuration=dm_api_configuration)
-    mailhog_api = MailhogApi(configuration=mailhog_configuration)
-    login = 'tus_test36'
+
+    account = DMApiAccount(configuration=dm_api_configuration)
+    mailhog = MailHogApi(configuration=mailhog_configuration)
+
+    login = 'tus_test38'
     password = '112233'
     email = f'{login}@mail.ru'
     json_data = {
@@ -34,11 +35,11 @@ def test_post_v1_account():
         'password': password,
     }
 
-    response = account_api.post_v1_account(json_data=json_data)
+    response = account.account_api.post_v1_account(json_data=json_data)
 
     assert response.status_code == 201, f'Пользователь не был создан {response.json()}'
     # Получить письма из почтового сервера
-    response = mailhog_api.get_api_v2_messages()
+    response = mailhog.mailhog_api.get_api_v2_messages()
 
     assert response.status_code == 200, 'Письма не были получены'
     # pprint.pprint(response.json())
@@ -49,7 +50,7 @@ def test_post_v1_account():
     # user_token = token
 
     # Активация пользователя
-    response = account_api.put_v1_account_token(token=token)
+    response = account.account_api.put_v1_account_token(token=token)
 
     assert response.status_code == 200, 'Пользователь не был активирован'
     # Авторизоваться
@@ -59,7 +60,7 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = login_api.post_v1_account_login(json_data=json_data)
+    response = account.login_api.post_v1_account_login(json_data=json_data)
 
     assert response.status_code == 200, 'Пользователь не смог авторизоваться'
 
