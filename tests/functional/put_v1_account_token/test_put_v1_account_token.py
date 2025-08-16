ff061e2655ab8192ffd17d550730fc335e2bc619
ustinov-gv <ustinov@exportcenter.ru>
2025-08-16 13:33:50 +0300

    фриз окружения

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -27,7 +27,7 @@ def test_put_v1_account_token():
 
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    login = 'tus2_test26'
+    login = 'tus2_test27'
     password = '112233'
     email = f'{login}@mail.ru'
 
