10796a46b45a60d31a9527acb99bf85517452644
ustinov-gv <ustinov@exportcenter.ru>
2025-09-11 23:53:40 +0300

    добавлен Dockerfile

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -23,8 +23,6 @@ def test_get_v1_account(auth_account_helper):
 
 
 
-
-
 def test_get_v1_account_no_auth(account_helper):
     with check_status_code_http(401, "User must be authenticated"):
         account_helper.dm_account_api.account_api.get_v1_account(validate_response=False)
